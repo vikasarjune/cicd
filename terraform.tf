57ff905af6ce5c7acd54d32a566419d2350df085
Gagandeep Singh <admin.gagan@gmail.com>
2021-10-23 12:46:49 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -10,7 +10,8 @@ resource "aws_instance" "app-server1" {
   command = "echo ${aws_instance.app-server1.public_ip} > ./inventory"
   }
   tags = {
-    Name = "gagan-server-devops"
+    Name = "gagan-server-devops-v1"
     dept = "Engineering"
+    env = "Dev"
   }
 }
