22907b011a7342bbf31b8747256c5b5d948fb21c
vikasarjune <48727169+vikasarjune@users.noreply.github.com>
2021-10-23 13:15:57 +0530

    added keypair

--- terraform.tf
+++ terraform.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "app-server1" {
   ami           = "ami-02e136e904f3da870"
   instance_type = "t2.micro"
-  key_name = "vodafone-devops"
+  key_name = "vikas-devops-cicd"
   provisioner "local-exec" {
   command = "echo ${aws_instance.app-server1.public_ip} > ./inventory"
   }
